4c220236bbf26d9304545e4a2b9746fad5578aa2
deathehra <jarvidniies@gmail.com>
2025-04-30 02:09:38 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+# Don't Remove Credit @TrytoliveAlon
+# Ask Doubt on telegram @Death_Support
+web: python3 bot.py
---
+++ plugins/Extra/openai.py
@@ -0,0 +1,13 @@
+# Don't Remove Credit @Trytolivealon
+# Ask Doubt on telegram @Death_Movie
+
+from pyrogram import Client, filters
+from plugins.Extra.engine import ask_ai
+
+
+@Client.on_message(filters.command('openai'))
+async def openai_ask(client, message):
+    if len(message.command) == 1:
+       return await message.reply_text("Give an input!")
+    m = await message.reply_text("👀")
+    await ask_ai(client, m, message)
